dc5d5e0c25a728dd144acbf033d279c32a297fc8
ahtez1 <oduorsm09@gmail.com>
2024-05-30 10:50:00 -0600

    Initial commit

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+
+    def ready(self):
+        import core.signals  # Importing signals to ensure they are loaded
