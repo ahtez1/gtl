2efdbd5b9282f4d418d14f4d43f54af3e913b09b
ahtez1 <162757194+ahtez1@users.noreply.github.com>
2025-02-16 23:42:34 -0700

    Update settings.py

--- gtls/settings.py
+++ gtls/settings.py
@@ -15,7 +15,21 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
+# Read .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# Secret Key
+SECRET_KEY = env('DJANGO_SECRET_KEY')
+
+# Debug Mode
+DEBUG = env('DJANGO_DEBUG')
+
+# Allowed Hosts
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
